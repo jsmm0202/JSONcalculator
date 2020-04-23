8b099881952ae0e10fa29ed3997e79f477583b8e
js_mm <jsmm0202@gmail.com>
2020-04-23 16:25:40 +0300

    adding more tests #2

--- Currency-calculator/Currency_calculator.Tests/CalculatorTest.cs
+++ Currency-calculator/Currency_calculator.Tests/CalculatorTest.cs
@@ -1,11 +1,10 @@
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using Xunit;
 
 namespace Currency_calculator.Tests
 {
-    public class UnitTest1
+    public class CalculatorTest
     {
         [Fact]
         public void Test_1plus2equals3()
@@ -69,6 +68,77 @@ namespace Currency_calculator.Tests
             }
 
             Assert.Equal("9", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+        }
+
+        [Fact]
+        public void Test_AllSteps()
+        {
+            var calculator = new Calculator();
+            string jsonState = null;
+
+            //1+2*4-2/5=2
+
+            jsonState = calculator.CalculateNextState(jsonState, "1");
+            Assert.Equal("1", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "+");
+            Assert.Equal("1", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "2");
+            Assert.Equal("2", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "*");
+            Assert.Equal("2", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "4");
+            Assert.Equal("4", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "-");
+            Assert.Equal("4", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "2");
+            Assert.Equal("2", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "/");
+            Assert.Equal("2", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "5");
+            Assert.Equal("5", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+
+            jsonState = calculator.CalculateNextState(jsonState, "=");
+            Assert.Equal("2", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+        }
+
+        [Fact]
+        public void Test_1plus2multiply4equals12()
+        {
+            var calculator = new Calculator();
+            string jsonState = null;
+
+            var inputs = new List<string>() { "1", "+", "2", "*", "4", "=" };
+
+            foreach (var input in inputs)
+            {
+                jsonState = calculator.CalculateNextState(jsonState, input);
+            }
+
+            Assert.Equal("12", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
+        }
+
+        [Fact]
+        public void Test_4545mul56minus567equals253953()
+        {
+            var calculator = new Calculator();
+            string jsonState = null;
+
+            var inputs = new List<string>() { "4", "5", "4", "5", "*", "5", "6", "-", "5", "6", "7", "=" };
+
+            foreach (var input in inputs)
+            {
+                jsonState = calculator.CalculateNextState(jsonState, input);
+            }
+
+            Assert.Equal("253953", JsonConvert.DeserializeObject<JsonState>(jsonState).Display);
         }
     }
 }
---
+++ Currency-calculator/Currency_calculator.Tests/ExeptionsTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xunit;
+
+namespace Currency_calculator.Tests
+{
+    public class ExeptionsTest
+    {
+        [Fact]
+        public void Test_InputMustBeOnlyOneCharacter()
+        {
+            var calculator = new Calculator();
+            string jsonState = null;
+
+            try
+            {
+                jsonState = calculator.CalculateNextState(jsonState, "11");
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal(@"11 is an invalid input.
+Input must be only one character", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void Test_InputMustBeDigitOrOperator()
+        {
+            var calculator = new Calculator();
+            string jsonState = null;
+
+            try
+            {
+                jsonState = calculator.CalculateNextState(jsonState, "a");
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal(@"a is an invalid input.
+Input must be a digit between 0-9 or one of the operators +-*/=", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void Test_TwoOperatorsMustBeSeparatedByNumbers()
+        {
+            var calculator = new Calculator();
+            string jsonState = null;
+            jsonState = calculator.CalculateNextState(jsonState, "1");
+            jsonState = calculator.CalculateNextState(jsonState, "+");
+            try
+            {
+                jsonState = calculator.CalculateNextState(jsonState, "-");
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal(@"Invalid input.
+Two operators must be separated by numbers.", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void Test_OperatorAndEqualsOperatorMustBeSeparatedByNumbers()
+        {
+            var calculator = new Calculator();
+            string jsonState = null;
+            jsonState = calculator.CalculateNextState(jsonState, "1");
+            jsonState = calculator.CalculateNextState(jsonState, "+");
+            try
+            {
+                jsonState = calculator.CalculateNextState(jsonState, "=");
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal(@"Invalid input.
+Two operators must be separated by numbers.", ex.Message);
+            }
+        }
+    }
+}
